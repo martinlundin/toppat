c866980863cd6a5a8bde3bcc1a3f8bba6271caca
Martin Lundin <mlundin1994@gmail.com>
2020-01-05 12:16:57 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 import { StateProvider } from './store.js';
 
 import Home from './components/Home';
-import Header from './components/Header';
 
 function App() {
-
   return (
     <StateProvider>
       <Home />
--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { store } from './../store'
 import TickerRow from './TickerRow'
 import Header from './Header'
 
 export default function LoggedIn() {
-  const global = React.useContext(store)
 
   return (
     <div>
